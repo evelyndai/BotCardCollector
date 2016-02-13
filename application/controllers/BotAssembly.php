8a702ac8e9dd4cd74c6d593405a8ff63dbf9a9c5
Justin Walk <justinwalk@live.com>
2016-02-13 13:42:02 -0800

    dropdown menus

--- application/controllers/BotAssembly.php
+++ application/controllers/BotAssembly.php
@@ -13,17 +13,17 @@ class BotAssembly extends Application {
 		$card_count = $this->collections->get_cards();
 		$card_count = $this->collections->sort_cards($card_count);
 
-		$top_cards = array('elevena' => $card_count['elevena0'],
-						   'elevenb' => $card_count['elevenb0'],
-					   	   'elevenc' => $card_count['elevenc0']);
+		$top_cards = array('Eleven-A' => $card_count['elevena0'],
+						   'Eleven-B' => $card_count['elevenb0'],
+					   	   'Eleven-C' => $card_count['elevenc0']);
 
-	    $mid_cards = array('elevena' => $card_count['elevena1'],
-   						   'elevenb' => $card_count['elevenb1'],
-   					   	   'elevenc' => $card_count['elevenc1']);
+	    $mid_cards = array('Eleven-A' => $card_count['elevena1'],
+   						   'Eleven-B' => $card_count['elevenb1'],
+   					   	   'Eleven-C' => $card_count['elevenc1']);
 
-	    $bot_cards = array('elevena' => $card_count['elevena2'],
-   						   'elevenb' => $card_count['elevenb2'],
-   					   	   'elevenc' => $card_count['elevenc2']);
+	    $bot_cards = array('Eleven-A' => $card_count['elevena2'],
+   						   'Eleven-B' => $card_count['elevenb2'],
+   					   	   'Eleven-C' => $card_count['elevenc2']);
 
 
 		$this->data['topcards'] = $top_cards;
--- application/views/bot_assembly.php
+++ application/views/bot_assembly.php
@@ -1,26 +1,73 @@
 
 <div id="something">
     <p> Build a Bot! </p>
-    <table>
+    <!-- <table>
         <tr>
             <th>A type</th>
             <th>B type</th>
             <th>C type</th>
         </tr>
         <tr>
-            <td><?php echo $topcards['elevena'];?></td>
-            <td><?php echo $topcards['elevenb'];?></td>
-            <td><?php echo $topcards['elevenc'];?></td>
+            <td><?php echo $topcards['Eleven-A'];?></td>
+            <td><?php echo $topcards['Eleven-B'];?></td>
+            <td><?php echo $topcards['Eleven-C'];?></td>
         </tr>
         <tr>
-            <td><?php echo $midcards['elevena'];?></td>
-            <td><?php echo $midcards['elevenb'];?></td>
-            <td><?php echo $midcards['elevenc'];?></td>
+            <td><?php echo $midcards['Eleven-A'];?></td>
+            <td><?php echo $midcards['Eleven-B'];?></td>
+            <td><?php echo $midcards['Eleven-C'];?></td>
         </tr>
         <tr>
-            <td><?php echo $botcards['elevena'];?></td>
-            <td><?php echo $botcards['elevenb'];?></td>
-            <td><?php echo $botcards['elevenc'];?></td>
+            <td><?php echo $botcards['Eleven-A'];?></td>
+            <td><?php echo $botcards['Eleven-B'];?></td>
+            <td><?php echo $botcards['Eleven-C'];?></td>
         </tr>
-    </table>
+    </table> -->
+    <select name="Top Pieces">
+        <option value=0>'Pick a top for your bot!'</option>
+        <?php
+        foreach($topcards as $key => $value)
+        {
+            if($value > 0)
+            {
+                echo "<option value=" . $key . ">" . $key . " (" . $value . ")</option>";
+            }
+        }
+        ?>
+    </select>
+
+    <select name="Middle Pieces">
+        <option value=0>'Pick a middle for your bot!'</option>
+        <?php
+        foreach($midcards as $key => $value)
+        {
+            if($value > 0)
+            {
+                echo "<option value=" . $key . ">" . $key . " (" . $value . ")</option>";
+            }
+        }
+        ?>
+    </select>
+
+    <select name="Bottom Pieces">
+        <option value=0>'Pick a bottom for your bot!'</option>
+        <?php
+        foreach($botcards as $key => $value)
+        {
+            if($value > 0)
+            {
+                echo "<option value=" . $key . ">" . $key . " (" . $value . ")</option>";
+            }
+        }
+        ?>
+    </select>
+    <button type="button">Assemble</button>
+</div>
+
+<div>
+    <img src="" id="bot_top" alt="Robot Top">
+    <br />
+    <img src="" id="bot_mid" alt="Robot Middle">
+    <br />
+    <img src="" id="bot_bot" alt="Robot Bottom">
 </div>
