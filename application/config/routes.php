6fcc34790a1bcc58856807bb677956401463fba9
evelyndai <daiyi68@live.cn>
2016-02-13 14:34:29 -0800

    Added holdings into Portfolio page

--- application/config/routes.php
+++ application/config/routes.php
@@ -54,3 +54,4 @@ $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
 $route['portfolio'] = 'portfolio';
 $route['factory'] = 'BotAssembly';
+$route['portfolio/(:any)'] = 'portfolio/getPort';
