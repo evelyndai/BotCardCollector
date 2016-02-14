e212713c4f222a91459b69b8f2a7eff607b3e485
jimmilin <jimmilin0531@gmail.com>
2016-02-13 17:22:32 -0800

    titlebar

--- application/views/_master_template.php
+++ application/views/_master_template.php
@@ -9,22 +9,25 @@ defined('BASEPATH') OR exit('No direct script access allowed');
         <meta HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=UTF-8"/>
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
         <link rel="stylesheet" type="text/css" href="/asset/style/base.css"/>
-<!--        This is the custome stylesheet that can potentially override the base stylesheet-->
+        <!--        This is the custome stylesheet that can potentially override the base stylesheet-->
         <link rel="stylesheet" type="text/css" href="{customCSS}"/>
 
     </head>
     <body>
         <div id="container">
-<!--            navigation bar-->
+            <!--            navigation bar-->
+            <div id="titlebar">
+                <img src="/asset/images/Home-image.png" /><h1>{title}</h1>
+            </div>
             <div id="navigation">
                 {menubar}
             </div>
-<!--content block-->
+            <!--content block-->
             <div id="content">
-                <h1>{title}</h1>
+               
                 {content}
             </div>
-<!--footer information-->
+            <!--footer information-->
             <div id="footer" class="span12">
                 Acit4850 Assignment by Casper Co, Evelyn Dai, Jimmi Lin, Justin Walk
             </div>
