6ee94410e358827f458881c9e08c591fe8d6a807
Casper Co <haln22111@gmail.com>
2016-04-14 16:15:31 -0700

    Finished dashboard

--- application/models/botserver.php
+++ application/models/botserver.php
@@ -66,5 +66,94 @@ class Botserver extends main_Model {
     $status = new SimpleXMLElement($handle);
     return $status->round;
   }
+
+  function get_transactions(){
+    //$handle = $this->botserver->php_get('/data/transactions');
+    $CsvString = $this->botserver->php_get("/data/transactions");
+    $Data = str_getcsv($CsvString, "\n"); //parse the rows
+    $rows = [];
+    $transactions = [];
+
+    foreach($Data as $Row)
+    {
+        $rows[] = str_getcsv($Row, ",");
+    }
+    if (count($rows) > 1 )
+    {
+        foreach ($rows as $row)
+        {
+            if ($row[2] == "b06")
+            {
+                $transactions[] = $row;
+            }
+        }
+    }
+
+    return $transactions;
+    //$transactions = new SimpleXMLElement($handle);
+    //return $handle;
+  }
+
+  function xml2array($xml){
+    $array = (array)$xml;
+    if (count($array) == 0) {
+      $array = (string)$xml;
+    }
+    if (is_array($array)) {
+      //recursive Parser
+      foreach ($array as $key => $value){
+        if (is_object($value)) {
+          if(strpos(get_class($value),"SimpleXML")!==false){
+            $array[$key] = SimpleXML2Array($value);
+          }
+        } else {
+          $array[$key] = SimpleXML2Array($value);
+        }
+      }
+    }
+    return $array;
+  }
+
+  function insertBuyTransaction($data){
+    $cardpack = new SimpleXMLElement($data);
+    $datetime = $cardpack->certificate[0]->datetime;
+    $player = $cardpack->certificate[0]->player;
+    insertTransaction($datetime, $player, 'x', 'buy');
+  }
+
+  function insertTransaction($datetime, $player, $series, $trans){
+    $data = array(
+      'datetime' => $datetime,
+      'player' => $player,
+      'series' => $series,
+      'trans' => $trans
+    );
+    $this->db->insert($data);
+  }
+
+  function get_collection(){
+    $CsvString = $this->botserver->php_get("/data/certificates");
+    $Data = str_getcsv($CsvString, "\n"); //parse the rows
+    $rows = [];
+    $equity = [];
+    $collection = [];
+
+    foreach($Data as $Row)
+    {
+      $rows[] = str_getcsv($Row, ",");
+    }
+    if (count($rows) > 1 )
+    {
+      foreach ($rows as $row)
+      {
+        if ($row[2] == "b06")
+        {
+          $collection[] = array("Player" => $row[3], "Piece" => $row[1], "Token" => $row[0], "Datetime" => $row[4]);
+        }
+      }
+    }
+
+    return $collection;
+  }
 }
 ?>
