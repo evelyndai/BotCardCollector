a55f5f0569d56675898ba67bd03b4ac72fd23151
Casper Co <haln22111@gmail.com>
2016-04-13 17:47:28 -0700

    Commenting updates and model update

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -27,46 +27,21 @@ class Welcome extends Application {
 		$gamestatus_title = "Game Status";
     $playerstatus_title = "Player Status";
 		$total_cards = 0;
-	  $collection_info = array();
-	  $cards_left = 0;
+		$cards_left = 0;
+
 	  $store_data = "";
 	  $series_data = "";
 	  $player_data = array();
 	  $player_store_data = "";
 
-
 		$players = $this->Gamestatus->get_players();
 		$series = $this->Gamestatus->get_series();
 		$collection = $this->Gamestatus->get_collection();
+		$cards_left = $this->Gamestatus->get_cards_left();
+		$total_cards = $this->Gamestatus->get_total_cards();
+		$collection_info = $this->Gamestatus->get_collection_info();
 
-		//Setup players array to hold equity information
-		for($x = 0; $x < count($players); $x++){
-			$players[$x]['Equity'] = 0;
-		}
-    for($x = 0; $x < count($series); $x++){
-      $series[$x]['Used'] = 0;
-      $total_cards = $total_cards + $series[$x]['Frequency'];
-    }
-    $cards_left = $total_cards;
-
-    foreach($collection as $record){
-      $cards_left = $cards_left - 1;
-      $coll_series = intval(substr($record['Piece'],0,2));
-      $serieskey = array_search($coll_series,array_column($series,'Series'));
-      $series[$serieskey]['Used'] = $series[$serieskey]['Used'] + 1;
-      $playerkey = array_search($record['Player'],array_column($players,'Player'));
-      $players[$playerkey]['Equity'] = $players[$playerkey]['Equity'] + $series[$serieskey]['Value'];
-      if($record['Player'] == $current_player){
-        if(empty($collection_info[$coll_series])){
-          $collection_info[$coll_series] = 1;
-        }
-        else{
-          $collection_info[$coll_series] = $collection_info[$coll_series] + 1;
-        }
-      }
-    }
-
-    //Create the information being viewed on the page
+		//Create the information being viewed on the page
     $store_data = "There are " . $cards_left . "/" . $total_cards . " cards left in the store.<br/>";
     foreach($series as $record){
       $record_left = $record['Frequency'] - $record['Used'];
