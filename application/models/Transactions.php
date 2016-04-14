3697a9c5d97f6c32990c982013f897698cdce68a
Justin Walk <justinwalk@live.com>
2016-04-14 11:34:18 -0700

    Update portfolio

--- application/models/Transactions.php
+++ application/models/Transactions.php
@@ -13,12 +13,12 @@ class Transactions extends main_Model2 {
     }
 
     function getTrans() {
-        
+
         $dataArray = array(
                 "team" => "B06",
                 "token" => "8d798b21a61aa0616d12050aedfdb64e",
                 "player" => 'Evelyn');
-            $method = $this->collections->php_post($dataArray, "/data/certificates");
+            $method = $this->botserver->php_post($dataArray, "/data/certificates");
         echo $method;
         $rows = explode("\n", $method);
 
@@ -34,7 +34,6 @@ class Transactions extends main_Model2 {
 // add an item to an order
 //    function getTrans($player){
 //        $query = $this->db->query('SELECT Player, DateTime, Series, Trans FROM transactions WHERE Player = "' . $player .'"');
-//      
+//
 //        return $query->result_array();
 //    }
-
