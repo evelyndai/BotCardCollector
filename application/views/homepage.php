0cec1c1f0bd294bc3330fb0329cdb3ddd4aa54dd
Casper Co <haln22111@gmail.com>
2016-04-14 10:26:16 -0700

    Fixed jquery and setup for more status info

--- application/views/homepage.php
+++ application/views/homepage.php
@@ -1,11 +1,12 @@
 <div class="gamestatus">
   <div class="title">{gamestatus_title}</div>
+  <div class="subtitle" id="round_number">{gamestatus_round}</div>
   {gamestatus}
 </div>
 <div class="playerstatus">
   <table class="table">
     <div class="title">{playerstatus_title}</div>
-    <tr class="playerrow_header">
+    <tr id="playerrow_header">
       <td class="td_header">Player</td>
       <td class="td_header">Peanuts</td>
       <td class="td_header">Equity</td>
@@ -18,4 +19,21 @@
     </tr>
     {/playerstatus}
 </table>
+</div>
+<div class="transactions">
+  <table class="table">
+    <div class="title">{transactions_title}</div>
+    <tr id="transactionsrow_header">
+      <td class="td_header">Player</td>
+      <td class="td_header">Action</td>
+      <td class="td_header">Series</td>
+    </tr>
+    {transactions}
+    <tr class="transactionsrow">
+      <td class="playername"><a href="{playerlink}">{playername}</a></td>
+      <td class="action">{action}</td>
+      <td class="transactionseries">{transaction_series}</td>
+    </tr>
+    {/transactions}
+  </table>
 </div>
